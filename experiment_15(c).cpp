7171c7a827fcb7281ac79a4f588a319c8713613e
navya <navyapkhajuria@gmail.com>
2025-09-15 15:13:49 +0530

    Create experiment_15(c).cpp

---
+++ experiment_15(c).cpp
@@ -0,0 +1,34 @@
+//Navya Pawan Khajuria
+//24070123151
+//B3
+
+#include <iostream>
+using namespace std;
+
+void printReverse(const string& str, int index) {
+    
+    if (index < 0)
+        return;
+
+    cout << str[index];
+
+    printReverse(str, index - 1);
+}
+
+int main() {
+    string input;
+
+    cout << "Enter a string: ";
+    getline(cin, input); 
+
+    cout << "Reversed string: ";
+    printReverse(input, input.length() - 1);
+    cout << endl;
+
+    return 0;
+}
+
+
+//Output:
+//Enter a string: navya
+//Reversed string: ayvan
